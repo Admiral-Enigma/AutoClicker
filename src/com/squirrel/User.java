9b42c71346311d1de4498f047585bb30abe84b02
Alexander <MrRandomgamerdkHD@gmail.com>
2015-09-30 21:10:51 +0200

    Code
    Alle the code for the program

---
+++ src/com/squirrel/User.java
@@ -0,0 +1,34 @@
+package com.squirrel;
+
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+
+public class User{
+	
+	public User(){
+		
+	}
+	
+	public void update(){
+		
+	}
+	
+	public void draw(Graphics2D g2d){
+		
+	}
+
+	public void keyPressed(KeyEvent e){
+		int key = e.getKeyCode();
+		if(key == KeyEvent.VK_G){
+			if(Gui.ShouldClick == true){
+				Gui.ShouldClick = false;
+			}else{
+				Gui.ShouldClick = true;
+			}
+		}
+	}
+	
+	public void keyReleased(KeyEvent e) {
+		
+	}
+}
